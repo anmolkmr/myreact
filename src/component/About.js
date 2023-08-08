76057e990b1bcc162189b78a75d150911ca9bc26
ANMOL KUMAR <anmolkmr028@gmail.com>
2023-08-08 14:24:51 +0530

    first react

---
+++ src/component/About.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function About() {
+  return (
+    <div>
+        About
+        <h1>I am your About</h1>
+        </div>
+  )
+}
---
+++ src/component/tempCodeRunnerFile.js
@@ -0,0 +1,7 @@
+import React from 'react'
+let arr=[{name:"ak",branch:"a"},{name:"anmol",branch:"b"},{name:"gaurav",branch:"c"},{name:"pal",branch:"d"}]
+export default function Demo22() {
+  return (
+    <div>Demo22</div>
+  )
+}
